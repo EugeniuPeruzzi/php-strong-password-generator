25e8c9c6e0065ef1a9dc6cb75139896106316742
EugeniuPeruzzi <jaka1997@live.it>
2023-07-05 17:54:40 +0200

    [milestone 1] fixed

--- index.php
+++ index.php
@@ -5,27 +5,17 @@
         $pass = []; 
         $charactersLength = strlen($characters) - 1;
         for ($i = 0; $i < $number; $i++) {
-            $n = rand(0, $alphaLength);
-            $pass[] = $alphabet[$n];
+            $n = rand(0, $charactersLength);
+            $pass[] = $characters[$n];
         }
         return implode($pass);
     }
-
-    if(isset($_GET['pswrd']) && (int)$_GET['pswrd'] !== 0) {
-        $userStrNum = $_GET['pswrd'];
-        echo $userStrNum;
-    } 
-    else{
-        echo 'sorry'; // di prova
-    } 
-
-
 
 ?>
 
 <!-- Milestone 1 Creare un form che invii in GET la lunghezza della password. OK
 Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. 
-Scriviamo tutto (logica e layout) in un unico file index.php
+Scriviamo tutto (logica e layout) in un unico file index.php OK
 Milestone 2 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
 Milestone 3 (BONUS) Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. 
 -->
@@ -38,18 +28,45 @@ Milestone 3 (BONUS) Invece di visualizzare la password nella index, effettuare u
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Pasword Generator</title>
 </head>
 
 <body>
-<form action="./index.php" method="GET">
-    <div class="mb-3">
-        <label for="pswd" class="form-label">Inserisci la lunghezza della tua password in numeri </label>
-        <input type="text" class="form-control" id="pswd" aria-describedby="pswdHelp" placeholder="Esempio: 23" name="pswrd">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- form -->
+                <div class="form-container w-25 ms-5 mt-5">
+                    <form action="./index.php" method="GET">
+                        <div class="mb-3">
+                            <label for="pswd" class="form-label">Inserisci la lunghezza della tua password in numeri </label>
+                            <input type="text" class="form-control" id="pswd" aria-describedby="pswdHelp" placeholder="Esempio: 23" name="pswrd">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                 </div>
+                 <!-- messaggio della password generata -->
+                <div class="message">
+                    <?php if(isset($_GET['pswrd']) && (int)$_GET['pswrd'] >= 5 && (int)$_GET['pswrd'] <= 175) { ?>
+                        <h2 class="ms-5 mt-5">La password rigenerata</h2>
+                        <div class="pswrd-box overflow-auto border rounded-2 p-4 ms-5 mt-2">
+                           <code> <?php echo  randomPswrd($_GET['pswrd']);?> </code>
+                        </div>
+                    <?php } else if ((isset($_GET['pswrd']) && (int)$_GET['pswrd'] <= 4)) { ?>
+                        <div class="">
+                            <?php echo '<div class="alert alert-warning mt-5 w-50 ms-5" role="alert">Il numero inserito deve essere superiore a 4</div>' ?>
+                        </div>
+                    <?php } else if ((isset($_GET['pswrd']) && (int)$_GET['pswrd'] >= 175)) {  ?>
+                        <div class="">
+                            <?php echo '<div class="alert alert-warning mt-5 w-50 ms-5" role="alert">Il numero inserito non puo essere superiore a 175</div>' ?>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+   
 </body>
 
 </html>
